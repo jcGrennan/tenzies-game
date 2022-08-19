3bc499cd616a7a58326da56792112ac28b7a38fb
JC Grennan <jgrennan94@gmail.com>
2022-08-19 17:42:34 +0100

    added comments

--- src/components/Roll.js
+++ src/components/Roll.js
@@ -1,15 +1,26 @@
-import React from "react"
+// composing and exporting Roll component
 
 export default function Roll(props) {
+
     return (
+
         <div className="roll">
+            
+            {/* using ternaries to determine whether the button rolls the dice or restarts the game */}
             <button className="roll-dice" onClick={props.tenzies ? props.newGame : props.rollDice}>
+
                 {props.tenzies ? "New Game" : "Roll"}
+
             </button>
+
             <div className="roll-scores">
+
                 <p>Rolls: {props.rollCount}</p>
                 <p>Best: {props.personalBest}</p>
+
             </div>
+
         </div>
+
     )
 }
